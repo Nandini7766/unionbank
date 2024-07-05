1c36a9ebe15411e727918bb0d66f4bf6de9c84b6
Nandini7766 <talarinandini7766@mail.com>
2024-07-05 10:20:54 +0530

    merge fixed

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,14 @@
+<<<<<<< HEAD
 hello i am upadating in dev branch..
 hello this file contains the deployment script..
 
 updating...
+=======
+stash demo ...
+stash start demo ..
+
+hello this file in dev branch
+hello this file contains the deployment script ..
+
+updating..
+>>>>>>> f12b8df (login feature)
